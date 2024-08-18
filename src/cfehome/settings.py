0b31fd01ed1893982fe0975434cc25183e5fdca7
AdityaMarne <adityamarne2001@gmail.com>
2024-08-19 00:46:45 +0530

    'second-commit'

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,8 +25,15 @@ SECRET_KEY = "django-insecure--k2mzb6$(81_af3_989712m5u28b#@)vc!91a9xaac5sv-wxy3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.railway.app' #https://saas.prod.railway.app
+]
 
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 # Application definition
 
